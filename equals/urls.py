5483b461fbed550e6a897cf23e61a7f9778b808f
Kerry Mitchell <kmitchell@sunlightfoundation.com>
2010-03-15 17:43:32 -0400

    updates

--- equals/urls.py
+++ equals/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     url(r'^events/$', 'django.views.generic.simple.direct_to_template', {"template": "events.html"}),
     url(r'^pledge/$', 'django.views.generic.simple.direct_to_template', {"template": "pledge.html"}),
+    url(r'^actnow/$', 'django.views.generic.simple.direct_to_template', {"template": "actnow.html"}),
     url(r'^groups/$', 'django.views.generic.simple.direct_to_template', {"template": "groups.html"}),
     url(r'^donate/$', 'django.views.generic.simple.direct_to_template', {"template": "local.html"}),   
     url(r'^toolkit/$', 'django.views.generic.simple.direct_to_template', {"template": "toolkit.html"}),
