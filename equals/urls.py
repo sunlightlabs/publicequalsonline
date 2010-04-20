fc34a7c0554452fc1b3812573d9b81ff66b63fa6
Kerry Mitchell <kmitchell@sunlightfoundation.com>
2010-04-20 16:57:18 -0400

    updated urls to allow for pledge adwords tracking

--- equals/urls.py
+++ equals/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     url(r'^events/$', 'django.views.generic.simple.direct_to_template', {"template": "events.html"}),
     url(r'^pledge/$', 'django.views.generic.simple.direct_to_template', {"template": "pledge.html"}),
-    url(r'^pledge/.*$', 'django.views.generic.simple.direct_to_template', {"template": "pledge.html"}),
     url(r'^pledge/thanks/$', 'django.views.generic.simple.direct_to_template', {"template": "pledge_thanks.html"}),
     url(r'^about/$', 'django.views.generic.simple.direct_to_template', {"template": "about.html"}, name="about"),
     url(r'^actnow/$', 'django.views.generic.simple.direct_to_template', {"template": "actnow.html"}),
@@ -14,6 +13,7 @@ urlpatterns = patterns('',
     url(r'^local/$', 'django.views.generic.simple.direct_to_template', {"template": "local.html"}),       
     url(r'^pledge/count/$', 'publicequalsonline.equals.views.get_count'),
     url(r'^pledge/2983gsoidfhd289dh/$', 'publicequalsonline.equals.views.increment_count'),
+    url(r'^pledge/.*$', 'django.views.generic.simple.direct_to_template', {"template": "pledge.html"}),    
     url(r'^news/$', 'publicequalsonline.equals.views.news', name="news"),
     
     
