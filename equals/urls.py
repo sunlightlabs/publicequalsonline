cb2684cacd85e50e279d310db65ca1ca0cb65499
Jeremy Carbaugh <jcarbaugh@gmail.com>
2010-06-23 15:42:47 -0400

    clean up templates

--- equals/urls.py
+++ equals/urls.py
@@ -1,38 +1,26 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    url(r'^events_old/$', 'django.views.generic.simple.direct_to_template', {"template": "events.html"}),
-    url(r'^pledge/$', 'django.views.generic.simple.direct_to_template', {"template": "pledge.html"}),
-    url(r'^pledge/thanks/$', 'django.views.generic.simple.direct_to_template', {"template": "pledge_thanks.html"}),
     url(r'^about/$', 'django.views.generic.simple.direct_to_template', {"template": "about.html"}, name="about"),
     url(r'^actnow/$', 'django.views.generic.simple.direct_to_template', {"template": "actnow.html"}),
-    url(r'^signup/$', 'django.views.generic.simple.direct_to_template', {"template": "account.html"}),
- #   url(r'^groups/$', 'django.views.generic.simple.direct_to_template', {"template": "groups.html"}),
-    url(r'^donate/$', 'django.views.generic.simple.direct_to_template', {"template": "donate.html"}),   
-    url(r'^toolkit/$', 'django.views.generic.simple.direct_to_template', {"template": "toolkit.html"}),
-    url(r'^local/$', 'django.views.generic.simple.direct_to_template', {"template": "local.html"}),       
+    url(r'^donate/$', 'django.views.generic.simple.direct_to_template', {"template": "donate.html"}),
+    #url(r'^events_old/$', 'django.views.generic.simple.direct_to_template', {"template": "events.html"}),
+    #url(r'^groups/$', 'django.views.generic.simple.direct_to_template', {"template": "groups.html"}),
+    #url(r'^local/$', 'django.views.generic.simple.direct_to_template', {"template": "local.html"}),
+    url(r'^news/$', 'publicequalsonline.equals.views.news', name="news"),
+    url(r'^pledge/$', 'django.views.generic.simple.direct_to_template', {"template": "pledge.html"}),
     url(r'^pledge/count/$', 'publicequalsonline.equals.views.get_count'),
     url(r'^pledge/2983gsoidfhd289dh/$', 'publicequalsonline.equals.views.increment_count'),
-    url(r'^pledge/.*$', 'django.views.generic.simple.direct_to_template', {"template": "pledge.html"}),    
+    url(r'^pledge/thanks/$', 'django.views.generic.simple.direct_to_template', {"template": "pledge_thanks.html"}),
+    url(r'^pledge/.*$', 'django.views.generic.simple.direct_to_template', {"template": "pledge.html"}),
+    #url(r'^signup/$', 'django.views.generic.simple.direct_to_template', {"template": "account.html"}),
+    #url(r'^toolkit/$', 'django.views.generic.simple.direct_to_template', {"template": "toolkit.html"}),  
     
     url(r'^events/events.kml', 'publicequalsonline.equals.views.generateEventKml'),
     url(r'^people/people.kml', 'publicequalsonline.equals.views.generatePeopleKml'),
-
-# Projects don't have location/geo data.
-#    url(r'^projects/projects.kml', 'publicequalsonline.equals.views.generateProjectKml'),
-    
-    url(r'^news/$', 'publicequalsonline.equals.views.news', name="news"),
-    
-    
-    
-            
-# TEMPORARILY
-#    url(r'^error/$', 'django.views.generic.simple.direct_to_template', {"template": "404.html"}), 
+    #url(r'^projects/projects.kml', 'publicequalsonline.equals.views.generateProjectKml'),
+        
+    #url(r'^error/$', 'django.views.generic.simple.direct_to_template', {"template": "404.html"}), 
 
     url(r'^$','publicequalsonline.equals.views.index'),
-)
-
-
-urlpatterns += patterns('publicequalsonline.equals.views',
-#    url(r'^$', 'index', name="home"),
 )
--- equals/views.py
+++ equals/views.py
@@ -1,39 +1,28 @@
 from django.contrib import auth
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect
-from django.template import RequestContext
-from django.shortcuts import render_to_response
-from django.template.loader import render_to_string
 from django.contrib.flatpages.views import flatpage
-# from blogdor.models import Post
-# from blogdor.views import archive
-from publicequalsonline.equals.models import FeaturedPost, PledgeCount
-from feedinator.models import FeedEntry
+from django.contrib.gis.shortcuts import render_to_kml
 from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.template.loader import render_to_string
 
 from anthill.projects.models import Project
 from anthill.events.models import Event
 from anthill.people.models import Profile
-
-from django.contrib.gis.shortcuts import render_to_kml
-
+from feedinator.models import FeedEntry
+from publicequalsonline.equals.models import FeaturedPost, PledgeCount
 
 def index(request):
-        try:
-            featured_blog = FeedEntry.objects.filter(feed__codename='upcoming_events').latest("date_published")
-        except FeedEntry.DoesNotExist:
-            featured_blog = None
-
-
-        feature_post = FeaturedPost.objects.filter(published=True).order_by("-date_published")[0:3]
-        context = {
+    try:
+        featured_blog = FeedEntry.objects.filter(feed__codename='upcoming_events').latest("date_published")
+    except FeedEntry.DoesNotExist:
+        featured_blog = None
+    feature_post = FeaturedPost.objects.filter(published=True).order_by("-date_published")[0:3]
+    context = {
         'feature_post': feature_post,
-         }
-
-        return render_to_response("index.html", context, context_instance=RequestContext(request))
- 
- 
-
+    }
+    return render_to_response("index.html", context, context_instance=RequestContext(request))
 
 def login(request):
         username = request.POST['username']
@@ -47,44 +36,19 @@ def login(request):
             return HttpResponseRedirect("/account/invalid/")
  
 def logout(request):
-            auth.logout(request)
-# Redirect to a success page.
-            return HttpResponseRedirect("/account/loggedout/")
- 
- 
- 
-    
-                             
-#def blog_wrapper(request):
-#    if 'feed' in request.GET:
-#        return HttpResponseRedirect('/blog/feeds/latest')
-#    return archive(request)                             
+    auth.logout(request)
+    # Redirect to a success page.
+    return HttpResponseRedirect("/account/loggedout/")
 
+#
+# utility views to make life easier
+#
 
-#def register(request):
-#    if request.method == 'POST': # If the form has been submitted...
-#        form = ContactForm(request.POST) # A form bound to the POST data
-#        if form.is_valid(): # All validation rules pass
-                # Process the data in form.cleaned_data
-                # ...
-#            return HttpResponseRedirect('/thanks/') # Redirect after POST
-#        else:
-#            form = ContactForm() # An unbound form
-
-#        return render_to_response('contact.html', {
-#            'form': form,
-#        })
-
-    #
-    # utility views to make life easier
-    #
-    
-    
 def page_not_found(request):
-            subject = "[404] publicequalsonline.com%s" % request.path 
-            message = "See subject for error"
-            #mail_admins(subject, message)
-            return defaults.page_not_found(request)
+    subject = "[404] publicequalsonline.com%s" % request.path 
+    message = "See subject for error"
+    #mail_admins(subject, message)
+    return defaults.page_not_found(request)
 
 def increment_count(request):
     PledgeCount.objects.all().update(count=F('count') + 1)
@@ -99,13 +63,6 @@ def news(request):
     feature_post = FeaturedPost.objects.filter(published=True).order_by("-date_published")[:20]
     data = { "feature_post": feature_post }
     return render_to_response("news.html", data, context_instance=RequestContext(request))
-
-
-#def meeting(request):
-#    return render_to_response("schedule.html")
-
-#def event_detail(request, id):
-#    return render_to_response("event.html")
        
 
 # THESE ARE FOR
